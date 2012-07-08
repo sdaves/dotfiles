350d150439fb0780dc10960b8b79389b6deef09b
Stephen Daves <stephen@stephendaves.com>
2012-07-08 18:24:18 -0400

    Added restore function, and migrated to use dotfiles.txt as a resource file.

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ import os
 
 class DotFileConfig(object):
     '''
-    Can set up and tear down your dotfile config.
+    Sets up and tears down your dotfile config.
     '''
     def __init__(self):
         os.chdir(os.path.abspath(os.path.dirname(__file__)))
@@ -23,22 +23,22 @@ class DotFileConfig(object):
         else:
             self.home_dir = os.path.abspath(self.home_dir)
 
-        self.backup_path = os.path.abspath('backup')
+        self.backup_link_path = os.path.abspath('backup_link')
+        self.backup_restore_path = os.path.abspath('backup_restore')
 
-        self.file_data = {
-            'vim/.vim': None,
-            'vim/.vimrc': None,
-            'screen/.screenrc': None,
-        }
+        self.file_data = ''.join(file('dotfiles.txt').readlines()).split(
+            '\n')[0:-1]
 
         item = ''.join(sys.argv[1:])
 
         if item in dir(self):
             getattr(self, item)()
-        elif not item:
-            print 'Please specify a command to perform.'
         else:
-            print 'Command not found. Here are the available commands:'
+            if item:
+                print 'Command not found. Here are the available commands:'
+            else:
+                print 'Please specify a command to perform.'
+
             print
 
             for item in dir(self):
@@ -55,17 +55,14 @@ class DotFileConfig(object):
         will be changesd, and lastly it creates symbolic links to each
         file needed.
         '''
-        if os.path.exists(self.backup_path):
-            print 'Backup directory exists, please remove it before '\
-                'running the setup.'
+        if os.path.exists(self.backup_link_path):
+            print 'Backup directory "%s" exists, please remove it, and '\
+                'run this command again.' % self.backup_link_path
         else:
-            os.mkdir(self.backup_path)
+            os.mkdir(self.backup_link_path)
             destination_path = self.home_dir
 
-            for key, item in self.file_data.iteritems():
-                if item is not None:
-                    destination_path = os.path.abspath(item)
-
+            for key in self.file_data:
                 if os.path.exists(destination_path):
                     file_name = os.path.split(key)[-1]
 
@@ -77,7 +74,7 @@ class DotFileConfig(object):
                         #backup link
                         os.link(
                             to_path,
-                            os.path.join(self.backup_path, file_name),
+                            os.path.join(self.backup_link_path, file_name),
                         )
 
                         #remove existing link
@@ -90,11 +87,48 @@ class DotFileConfig(object):
         Tears down your dotfile config by first removing the previously
         created symbolic links, and lastly restoring any backups.
         '''
-        raise Exception('todo')
+        if os.path.exists(self.backup_restore_path):
+            print 'Backup directory "%s" exists, please remove it, and '\
+                'run this command again.' % self.backup_restore_path
+            return
+        else:
+            os.mkdir(self.backup_restore_path)
+
+        if not os.path.exists(self.backup_link_path):
+            return
+
+        for key in self.file_data:
+            destination_path = self.home_dir
+
+            if os.path.exists(destination_path):
+                file_name = os.path.split(key)[-1]
+
+                to_path = os.path.abspath(
+                    os.path.join(destination_path, file_name),
+                )
+
+                if os.path.isfile(to_path) or os.path.islink(to_path):
+                    #backup link
+                    os.link(
+                        to_path,
+                        os.path.join(self.backup_restore_path, file_name),
+                    )
+
+                    #remove existing link
+                    os.unlink(to_path)
+
+                os.link(
+                    os.path.join(self.backup_link_path, file_name),
+                    to_path,
+                )
 
 
 def main():
+    '''
+    Runs the main application.
+    '''
     DotFileConfig()
+
 
 if __name__ == '__main__':
     main()
